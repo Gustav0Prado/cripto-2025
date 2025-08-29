77325ab247a58da5faceac68c9ed71ff3f302b01
gustavops <guprado.silva@gmail.com>
2025-08-29 10:55:51 -0300

    [add] Cifra de substituição estilo Vigenere/One time pad

--- T1/main.py
+++ T1/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-import argparse, aes, nova
+import argparse, aes, rlyeh
 
 parse = argparse.ArgumentParser(description="Argumentos para operação dos algoritmos")
 
 parse.add_argument('-c', '--plain_text', type=str, help="Arquivo de Texto Claro")
 parse.add_argument('-d', '--cypher_text', type=str, help="Arquivo de Texto Cifrado")
 parse.add_argument('-a', '--aes', action='store_true', help="Usar AES para encriptação")
-parse.add_argument('-n', '--nova', action='store_true', help="Usar cifra Nova para encriptação")
+parse.add_argument('-r', '--rlyeh', action='store_true', help="Usar cifra R'lyeh para encriptação")
 parse.add_argument('-s', '--output_file', type=str, help="Arquivo de Saída", required=True)
 
 args = parse.parse_args()
@@ -15,7 +15,7 @@ args = parse.parse_args()
 if not args.plain_text and not args.cypher_text:
     raise ValueError("Insira um arquivo de texto claro (opção -c) ou um arquivo de texto criptografado (opção -d)")
 
-if not args.aes and not args.nova:
+if not args.aes and not args.rlyeh:
     raise ValueError("Defina qual cifra usar com -a (AES) ou -n (Nova)")
 
 key = input("Insira sua chave: ")
@@ -25,11 +25,11 @@ if args.plain_text:
     if args.aes:
         time = aes.encrypt(args.plain_text, args.output_file, key)
     else:
-        time = nova.encrypt(args.plain_text, args.output_file, key)
+        time = rlyeh.encrypt(args.plain_text, args.output_file, key)
 elif args.cypher_text:
     if args.aes:
         time = aes.decrypt(args.cypher_text, args.output_file, key)
     else:
-        time = nova.decrypt(args.cypher_text, args.output_file, key)
+        time = rlyeh.decrypt(args.cypher_text, args.output_file, key)
 
 print(f'Tempo decorrido: {time:.5f}s')
---
+++ T1/rlyeh.py
@@ -0,0 +1,60 @@
+import time
+
+def to_ord(c: str) -> str:
+    return str(f'{ord(c):04d}')
+
+def to_char(c: str) -> str:
+    return chr(int(c[0]+c[1]+c[2]+c[3])-2)
+
+def encrypt(input: str, output: str, key: str) -> float:
+    time_exec: float = time.time()
+
+    # Chave precisa ser > 0
+    k_index: int = 0
+    k_length: int = len(key)
+    if len(key) <= 0:
+        return (time.time() - time_exec)
+
+    with open(output, '+w') as output_file:
+        with open(input, 'r') as input_file:
+            for line in input_file:
+                for char in line:
+                    new_char: str = chr(ord(char) + ord(key[k_index]))
+                    
+                    # Repete chave até o tamanho do texto
+                    k_index += 1
+                    if k_index >= k_length:
+                        k_index = 0
+
+                    # Escreve na saida
+                    output_file.write(new_char)
+                
+
+    time_exec = (time.time() - time_exec)
+    return time_exec
+
+def decrypt(input: str, output: str, key: str) -> float:
+    time_exec: float = time.time()
+
+    # Chave precisa ser > 0
+    k_index: int = 0
+    k_length: int = len(key)
+    if len(key) <= 0:
+        return (time.time() - time_exec)
+
+    with open(output, '+w') as output_file:
+        with open(input, 'r') as input_file:
+            for line in input_file:
+                for char in line:
+                    new_char: str = chr(ord(char) - ord(key[k_index]))
+
+                    # Repete chave até o tamanho do texto
+                    k_index += 1
+                    if k_index >= k_length:
+                        k_index = 0
+
+                    # Escreve na saida
+                    output_file.write(new_char)
+
+    time_exec = (time.time() - time_exec)
+    return time_exec
