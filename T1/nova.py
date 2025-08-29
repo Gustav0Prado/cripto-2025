4d6e3f1a0f3409bab6450d76d8574cfeaf55b0c9
gustavops <guprado.silva@gmail.com>
2025-08-29 10:07:01 -0300

    fix(file): Fix clear_text encoding to UTF-8 and add to_char/to_ord functions in nova

--- T1/nova.py
+++ T1/nova.py
@@ -1,13 +1,20 @@
 import time
 
+def to_ord(c: str) -> str:
+    return str(f'{ord(c):04d}')
+
+def to_char(c: str) -> str:
+    return chr(int(c[0]+c[1]+c[2]+c[3]))
+
 def encrypt(input: str, output: str, key: str) -> float:
     time_exec = time.time()
 
     with open(output, '+w') as output_file:
         with open(input, 'r') as input_file:
-            line = input_file.readline().strip('\n')
-            for char in line:
-                output_file.write(char)
+            for line in input_file:
+                for char in line:
+                    output_file.write(to_ord(char))
+                
 
     time_exec = (time.time() - time_exec)
     return time_exec
@@ -15,11 +22,14 @@ def encrypt(input: str, output: str, key: str) -> float:
 def decrypt(input: str, output: str, key: str) -> float:
     time_exec = time.time()
 
+    STEP = 4
+
     with open(output, '+w') as output_file:
         with open(input, 'r') as input_file:
-            line = input_file.readline().strip('\n')
-            for char in line:
-                output_file.write(char)
+            for line in input_file:
+                for i in range(0, len(line), STEP):
+                    num = line[i:i+STEP]
+                    output_file.write(to_char(num))
 
     time_exec = (time.time() - time_exec)
     return time_exec
