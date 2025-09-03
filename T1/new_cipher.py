a8e97b9feff5a46b1fee162bbc5a7dcfd8517e0c
gustavops <guprado.silva@gmail.com>
2025-09-03 11:08:37 -0300

    [fix] Cifra de substituição funcionando

--- T1/new_cipher.py
+++ T1/new_cipher.py
@@ -2,38 +2,28 @@ import random, time
 
 UTF8_MAX: int = 11263
 
-def substitute_enc(text:str) -> str:
+def substitute(text:str) -> str:
     out = ""
     for char in text:
         randint = random.randrange(0, UTF8_MAX)
-        out += chr( ord(char) + randint )
+        out += chr( ord(char) ^ randint )
 
     return out
-
-def substitute_dec(text: str) -> str:
-    out = ""
-    
-    for char in text:
-        randint = random.randrange(0, UTF8_MAX)
-        out += chr(abs(ord(char) -  randint))
-
-    return out
-
 
 def encrypt(input: str, output: str, key: str) -> float:
     time_exec = time.perf_counter()
     
     random.seed(key)
 
-    with open(input, 'r') as input_file:
-        with open(output, 'w') as output_file:
+    with open(output, 'w') as output_file:
+        with open(input, 'r') as input_file:
             for line in input_file:
-                subs = substitute_enc(line)
+                subs = substitute(line)
 
-                # Transposição aqui
+                # Transposição aqui!!
                 # ciphered = subs + transp
                 ciphered = subs
-
+                
                 output_file.write(ciphered)
 
     return (time.perf_counter() - time_exec)
@@ -43,15 +33,15 @@ def decrypt(input: str, output: str, key: str) -> float:
     
     random.seed(key)
 
-    with open(input, 'r') as input_file:
-        with open(output, 'w') as output_file:
+    with open(output, 'w') as output_file:
+        with open(input, 'r') as input_file:
             for line in input_file:
-                subs = substitute_dec(line)
+                subs = substitute(line)
 
-                # Transposição aqui
+                # Transposição aqui!!
                 # ciphered = subs + transp
                 ciphered = subs
-
+                
                 output_file.write(ciphered)
 
     return (time.perf_counter() - time_exec)
