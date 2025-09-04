186f3500c0f66bbc768252c04d9bb9c0e66628bb
NARizzardi <rizzanic2000@gmail.com>
2025-09-03 23:26:51 -0300

    add encrypt do transposition funcionando (?)
    Signed-off-by: NARizzardi <rizzanic2000@gmail.com>

--- T1/columnar.py
+++ T1/columnar.py
@@ -1,10 +1,44 @@
-import time
+import time, string, random
+
+def create_matrix(text: str, length: int) -> list[list[str]]:
+    matrix = []
+    remainder = length - (len(text) % length)
+    for i in range(0,remainder):
+        text += random.choice(string.ascii_letters)
+    while text:
+        line = []
+        for i in range(0,length):
+            line.append(text[0])
+            text = text[1:]
+        matrix.append(line)
+    return matrix
+
+def cipher_text(matrix: list[list[str]], key: str) -> str:
+    sl = sorted(enumerate(key), key=lambda x: x[1])
+    text = ""
+    for i in range(0, len(matrix)):
+        new_line = ""
+        for j in range(0, len(key)):
+            new_line += matrix[i][sl[j][0]]
+        text += new_line
+
+    return text
 
 def encrypt(input: str, output: str, first_key: str, second_key: str) -> float:
     time_exec = time.perf_counter()
-    # remove espacos do testo limpo
-    # colocar o texto na matriz, o numero de linhas vai aumentar dinamicamente
-    # o numero de colunas se da com base no comprimento da palavra
+    first_len = len(first_key)
+    second_len = len(second_key)
+    with open(output, 'w') as output_file:
+        with open(input, 'r') as input_file:
+            text = input_file.read()
+
+            transposition_matrix = create_matrix(text, first_len)
+            transpose_text = cipher_text(transposition_matrix, first_key)
+
+            second_transpose = create_matrix(transpose_text, second_len)
+            ciphered_text = cipher_text(second_transpose, second_key)
+            output_file.write(ciphered_text)
+
     return time.perf_counter() - time_exec;
 
 def decrypt(input: str, output: str, first_key: str, second_key: str) -> float:
--- T1/main.py
+++ T1/main.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 
-import argparse, aes, new_cipher
+import argparse, aes, new_cipher, columnar
 
 parse = argparse.ArgumentParser(description="Argumentos para operação dos algoritmos")
 parse.add_argument('-c', '--plain_text', type=str, help="Arquivo de Texto Claro")
 parse.add_argument('-d', '--cypher_text', type=str, help="Arquivo de Texto Cifrado")
 parse.add_argument('-a', '--aes', action='store_true', help="Usar AES para encriptação")
-parse.add_argument('-n', '--new', action='store_true', help="Usar ? para encriptação")
 parse.add_argument('-o', '--output_file', type=str, help="Arquivo de Saída", required=True)
+parse.add_argument('-s', '--substitution', action='store_true', help="Cifrar por substituição")
+parse.add_argument('-t', '--transposition', action='store_true', help="Cifrar por transposição")
 
 args = parse.parse_args()
 
 if not args.plain_text and not args.cypher_text:
     raise ValueError("Insira um arquivo de texto claro (opção -c) ou um arquivo de texto criptografado (opção -d)")
 
-if not args.aes and not args.new:
-    raise ValueError("Defina qual cifra usar com -a (AES) ou -n (??)")
+if not args.aes and not args.substitution and not args.transposition:
+    raise ValueError("Defina qual cifra usar com -a (AES) ou -s (??) ou -t (Transposição Colunar)")
 
 key: str = input("Insira sua chave: ")
 
@@ -23,12 +24,18 @@ time: float = 0.0
 if args.plain_text:
     if args.aes:
         time = aes.encrypt(args.plain_text, args.output_file, key)
-    elif args.new:
+    elif args.substitution:
         time = new_cipher.encrypt(args.plain_text, args.output_file, key)
+    elif args.transposition:
+        second_key: str = input("Insira sua segunda chave: ")
+        time = columnar.encrypt(args.plain_text, args.output_file, key, second_key)
 elif args.cypher_text:
     if args.aes:
         time = aes.decrypt(args.cypher_text, args.output_file, key)
-    elif args.new:
+    elif args.substitution:
         time = new_cipher.encrypt(args.cypher_text, args.output_file, key)
+    elif args.transposition:
+        second_key: str = input("Insira sua segunda chave: ")
+        time = columnar.encrypt(args.plain_text, args.output_file, "biscoito", "bolacha")
 
 print(f'Tempo decorrido: {time:.5f}s')
