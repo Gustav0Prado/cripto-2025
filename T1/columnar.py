1acee2ea83ec187aa2c20ac6f12898177d7403a3
Nicolas Rizzardi <rizzanic2000@gmail.com>
2025-09-29 13:00:28 -0300

    add flag de benchmark
    Signed-off-by: Nicolas Rizzardi <rizzanic2000@gmail.com>

--- T1/columnar.py
+++ T1/columnar.py
@@ -3,50 +3,30 @@ import time
 ########################### ENCRYPT ###########################
 ########################## FUNCTIONS ##########################
 
+
 def create_matrix(text: str, key: str) -> list[list[str]]:
     key_len = len(key)
-    matrix = []
 
-    remainder = (len(text) % key_len)
-    if(remainder != 0):
-        for i in range(remainder, key_len):
-            text += ' '
+    remainder = len(text) % key_len
+    if remainder != 0:
+       text += " " * (key_len - remainder)
 
-    while text:
-        line = []
-        for i in range(key_len):
-            line.append(text[0])
-            text = text[1:]
-        matrix.append(line)
-
-    return matrix
+    return [list(text[i:i + key_len]) for i in range(0, len(text), key_len)]
 
 def rebuild_ciphered_text(matrix: list[list[str]], key: str) -> str:
-    sl = sorted(enumerate(key), key=lambda x: x[1])
-    text = ''.join(
-        str(matrix[i][sl[j][0]])
-        for j in range(len(key))
-        for i in range(len(matrix))
-    )
+    order = [i for i, _ in sorted(enumerate(key), key=lambda x: x[1])]
 
-    return text
+    return "".join(matrix[row][col] for col in order for row in range(len(matrix)))
 
-def encrypt(input: str, output: str, first_key: str, second_key: str, third_key: str) -> float:
+def encrypt(input: str, output: str, first_key: str, second_key: str) -> float:
     time_exec = time.perf_counter()
-    with open(output, 'w') as output_file:
-        with open(input, 'r') as input_file:
-            text = input_file.read()
+    with open(output, 'w') as output_file, open(input, 'r') as input_file:
+        text = input_file.read()
 
-            transposition_matrix = create_matrix(text, first_key)
-            transpose_text = rebuild_ciphered_text(transposition_matrix, first_key)
+        transpose_text = rebuild_ciphered_text(create_matrix(text, first_key), first_key)
+        ciphered_text = rebuild_ciphered_text(create_matrix(transpose_text, second_key), second_key)
 
-            second_transpose = create_matrix(transpose_text, second_key)
-            second_transpose_text = rebuild_ciphered_text(second_transpose, second_key)
-
-            third_transpose = create_matrix(second_transpose_text, third_key)
-            ciphered_text = rebuild_ciphered_text(third_transpose, third_key)
-
-            output_file.write(ciphered_text)
+        output_file.write(ciphered_text)
 
     return time.perf_counter() - time_exec
 
@@ -54,56 +34,34 @@ def encrypt(input: str, output: str, first_key: str, second_key: str, third_key:
 ########################## FUNCTIONS ##########################
 
 def reorder_matrix(matrix: list[list[str]], sl: list[tuple[int, str]], key_len: int) -> list[list[str]]:
-    ordered_matrix = []
-    for i in range(key_len):
-        index = next(idx for idx, (orig_idx, _) in enumerate(sl) if orig_idx == i)
-        ordered_matrix.append(matrix[index])
-
-    return ordered_matrix
+    index_map = {orig_idx: idx for idx, (orig_idx, _) in enumerate(sl)}
+    return [matrix[index_map[i]] for i in range(key_len)]
 
 def create_transpose_matrix(text: str, key: str) -> list[list[str]]:
     key_len = len(key)
-    matrix = []
-    remainder = (len(text) % key_len)
-    if(remainder != 0):
-        for i in range(remainder, key_len):
-            text += ' '
 
-    columns = (len(text) // key_len)
-    while text:
-        line = []
-        for i in range(columns):
-            line.append(text[0])
-            text = text[1:]
-        matrix.append(line)
+    remainder = len(text) % key_len
+    if remainder != 0:
+        text += " " * (key_len - remainder)
+
+    columns = len(text) // key_len
+    matrix = [list(text[i:i + columns]) for i in range(0, len(text), columns)]
 
     sl = sorted(enumerate(key), key=lambda x: x[1])
-    reordered_matrix = reorder_matrix(matrix, sl, len(key))
+    reordered_matrix = reorder_matrix(matrix, sl, key_len)
 
-    transposed_matrix = [list(row) for row in zip(*reordered_matrix)]
-    return transposed_matrix
+    return [list(row) for row in zip(*reordered_matrix)]
 
 def rebuild_deciphered_text(matrix: list[list[str]], key: str ) -> str:
-    text = ''.join(
-        [str(matrix[i][j])
-         for i in range(len(matrix))
-         for j in range(len(matrix[i]))]
-    ).rstrip()
-    return text
+    return "".join(ch for row in matrix for ch in row).rstrip()
 
-def decrypt(input: str, output: str, third_key: str, second_key: str, first_key: str) -> float:
+def decrypt(input: str, output: str, second_key: str, first_key: str) -> float:
     time_exec = time.perf_counter()
-    with open(output, 'w') as output_file:
-        with open(input, 'r') as input_file:
+    with open(output, 'w') as output_file, open(input, 'r') as input_file:
             text = input_file.read()
-            transposition_matrix = create_transpose_matrix(text, first_key)
-            transpose_text = rebuild_deciphered_text(transposition_matrix, first_key)
-
-            second_transpose = create_transpose_matrix(transpose_text, second_key)
-            second_transpose_text = rebuild_deciphered_text(second_transpose, second_key)
-
-            third_transpose = create_transpose_matrix(second_transpose_text, third_key)
-            deciphered_text = rebuild_deciphered_text(third_transpose, third_key)
+            
+            transpose_text = rebuild_deciphered_text(create_transpose_matrix(text, first_key), first_key)
+            deciphered_text = rebuild_deciphered_text(create_transpose_matrix(transpose_text, second_key), second_key)
 
             output_file.write(deciphered_text)
     return time.perf_counter() - time_exec
--- T1/main.py
+++ T1/main.py
@@ -1,16 +1,85 @@
 #!/usr/bin/python3
 
 import argparse, aes, substitution, columnar, os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 parse = argparse.ArgumentParser(description="Argumentos para operação dos algoritmos")
 parse.add_argument('-c', '--plain_text', type=str, help="Arquivo de Texto Claro")
 parse.add_argument('-d', '--cypher_text', type=str, help="Arquivo de Texto Cifrado")
 parse.add_argument('-a', '--aes', action='store_true', help="Usar AES para encriptação")
-parse.add_argument('-o', '--output_file', type=str, help="Arquivo de Saída", required=True)
+parse.add_argument('-o', '--output_file', type=str, help="Arquivo de Saída")
 parse.add_argument('-s', '--substitution', action='store_true', help="Cifrar por substituição")
 parse.add_argument('-t', '--transposition', action='store_true', help="Cifrar por transposição")
+parse.add_argument('-b', '--benchmarking', action='store_true', help='Modelo para benchmarking')
 
 args = parse.parse_args()
+
+
+if args.benchmarking:
+    def create_plot(df, title, filename):
+        plt.figure(figsize=(12, 8))
+        bars = plt.bar(df['Operation'], df['Time (seconds)'], color=['skyblue', 'lightcoral', 'lightgreen', 'orange'])
+        plt.title(f'Encryption/Decryption Times - {title}', fontsize=14)
+        plt.xlabel('Operation', fontsize=12)
+        plt.ylabel('Time (seconds) - Log Scale', fontsize=12)
+        plt.yscale('log')
+        plt.xticks(rotation=45)
+        
+        for bar, value in zip(bars, df['Time (seconds)']):
+            plt.text(bar.get_x() + bar.get_width()/2, bar.get_height(), 
+                    f'{value:.4f}', ha='center', va='bottom', fontsize=10)
+        
+        plt.grid(True, alpha=0.3, axis='y')
+        plt.tight_layout()
+        plt.savefig(filename, dpi=300, bbox_inches='tight')
+        plt.close()
+        print(f"Gráfico salvo como: {filename}.png")
+
+    files_list = ['./clear_text/1-LoremIpsum.txt', './clear_text/2-Frankenstein.txt', './clear_text/3-CompleteWorksShakespeare.txt']
+
+    key: str = input("Insira sua chave: ")
+    second_key: str = input("Insira sua segunda chave: ")
+    times = [[],[],[]]
+    i = 0
+    for file in files_list:
+        aes_encrypt_file = file+'.aes'
+        aes_decrypt_file = file+'.aes.dec'
+        students_encrypt_file = file+'.est'
+        students_decrypt_file = file+'.est.dec'
+
+        times[i].append(('AES - Enc', aes.encrypt(file, aes_encrypt_file, key)))
+        times[i].append(('AES - Dec', aes.decrypt(aes_encrypt_file, aes_decrypt_file, key)))
+
+        encrypt_time = columnar.encrypt(file, 'intermed.txt', key, second_key)
+        encrypt_time += substitution.encrypt('intermed.txt', students_encrypt_file, key)
+        times[i].append(('Students - Enc', encrypt_time))
+
+        decrypt_time = substitution.decrypt(students_encrypt_file, 'intermed.txt',  key)
+        decrypt_time += columnar.decrypt('intermed.txt', students_decrypt_file, key, second_key)
+        times[i].append(('Students - Dec', decrypt_time))
+        i += 1
+
+    df1 = pd.DataFrame(times[0], columns=['Operation', 'Time (seconds)'])
+    df1['File'] = files_list[0]
+
+    df2 = pd.DataFrame(times[1], columns=['Operation', 'Time (seconds)'])
+    df2['File'] = files_list[1]
+
+    df3 = pd.DataFrame(times[2], columns=['Operation', 'Time (seconds)'])
+    df3['File'] = files_list[2]
+
+    create_plot(df1, "LoremIpsum", "10kb")
+    create_plot(df2, "Frankenstein", "500kb")
+    create_plot(df3, "Complete Works Shakespeare", "5mb")
+
+    print(times)
+    os.remove('intermed.txt')
+    exit(0)
+
+
+if not args.output_file:
+    raise ValueError("Insira um arquivo de saída para o programa")
 
 if not args.plain_text and not args.cypher_text:
     raise ValueError("Insira um arquivo de texto claro (opção -c) ou um arquivo de texto criptografado (opção -d)")
@@ -29,9 +98,8 @@ if args.plain_text:
     # Substituição + transposição
     if args.substitution and args.transposition:
         second_key: str = input("Insira sua segunda chave: ")
-        third_key: str = input("Insira sua terceira chave: ")
-        time = substitution.encrypt(args.plain_text, "intermed.txt", key)
-        time += columnar.encrypt("intermed.txt", args.output_file, key, second_key, third_key)
+        time = columnar.encrypt(args.plain_text, "intermed.txt", key, second_key)
+        time += substitution.encrypt("intermed.txt", args.output_file, key)
         os.remove("intermed.txt")
 
     # Só substituição
@@ -41,8 +109,7 @@ if args.plain_text:
     # Só transposição
     elif args.transposition:
         second_key: str = input("Insira sua segunda chave: ")
-        third_key: str = input("Insira sua terceira chave: ")
-        time = columnar.encrypt(args.plain_text, args.output_file, key, second_key, third_key)
+        time = columnar.encrypt(args.plain_text, args.output_file, key, second_key)
 
 elif args.cypher_text:
     # Algoritmo AES
@@ -52,9 +119,8 @@ elif args.cypher_text:
     # Substituição + transposição
     if args.substitution and args.transposition:
         second_key: str = input("Insira sua segunda chave: ")
-        third_key: str = input("Insira sua terceira chave: ")
-        time = columnar.decrypt(args.cypher_text, "intermed.txt", key, second_key, third_key)
-        time += substitution.decrypt("intermed.txt", args.output_file, key)
+        time = substitution.decrypt(args.cypher_text, "intermed.txt", key)
+        time += columnar.decrypt("intermed.txt", args.output_file, key, second_key)
         os.remove("intermed.txt")
     
     # Só substituição
@@ -64,7 +130,6 @@ elif args.cypher_text:
     # Só transposição
     elif args.transposition:
         second_key: str = input("Insira sua segunda chave: ")
-        third_key: str = input("Insira sua terceira chave: ")
-        time = columnar.decrypt(args.cypher_text, args.output_file, key, second_key, third_key)
+        time = columnar.decrypt(args.cypher_text, args.output_file, key, second_key)
 
 print(f'Tempo decorrido: {time:.5f}s')
